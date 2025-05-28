b006576c7c51a8b11f5980a1c06a50e114c729f9
Sofi Kutter <sofikutter01@gmail.com>
2025-05-27 23:13:10 -0300

    Modificaciones entrega 27

--- src/App.svelte
+++ src/App.svelte
@@ -246,11 +246,11 @@ onMount(async () => {
             <h3 class="legend-title">Grado de satisfacción</h3>
             <div class="legend-vertical-list">
               <div class="legend-icon-horizontal">
-                <WavyLineIcon size={75} />
+                <WavyLineIcon size={80} />
                 <p>Satisfecho</p>
               </div>
               <div class="legend-icon-horizontal">
-                <WavyLineHueco size={75} />
+                <WavyLineHueco height={100} size={80} />
                 <p>Insatisfecho</p>
               </div>
             </div>
@@ -265,7 +265,7 @@ onMount(async () => {
                 <StarIcon size={70} />
               </div>
               <p class="commit-description">
-                El tamaño varia según la cantidad de commits realizados en el repositorio.
+                El tamaño varia según la cantidad de commits realizados.
               </p>
             </div>
           </div>
